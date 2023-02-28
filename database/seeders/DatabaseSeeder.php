2cf7773b32493f90e78ccef69f0d021d3a23d921
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-02-28 22:48:08 +0700

    chore: main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(ApplicationSeeder::class);
+        $this->call(LanguageSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(AuthorSeeder::class);
+        $this->call(BookCategorySeeder::class);
+        $this->call(PublisherSeeder::class);
+        $this->call(BookSeeder::class);
+        $this->call(GradeSeeder::class);
         $this->call(UserSeeder::class);
     }
 }
