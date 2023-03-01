d8acdd23f64476077c6cea690f1540babfd718e2
Sleepy4k <sarahpalastrin@gmail.com>
2023-03-01 14:23:54 +0700

    chore: some service provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -13,7 +13,18 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Bind the Repository Interface to the Repository Implementation
+        $this->app->bind('App\Contracts\EloquentInterface', 'App\Repositories\EloquentRepository');
+        $this->app->bind('App\Contracts\Models\RoleInterface', 'App\Repositories\Models\RoleRepository');
+        $this->app->bind('App\Contracts\Models\UserInterface', 'App\Repositories\Models\UserRepository');
+        $this->app->bind('App\Contracts\Models\BookInterface', 'App\Repositories\Models\BookRepository');
+        $this->app->bind('App\Contracts\Models\AuditInterface', 'App\Repositories\Models\AuditRepository');
+        $this->app->bind('App\Contracts\Models\GradeInterface', 'App\Repositories\Models\GradeRepository');
+        $this->app->bind('App\Contracts\Models\AuthorInterface', 'App\Repositories\Models\AuthorRepository');
+        $this->app->bind('App\Contracts\Models\LanguageInterface', 'App\Repositories\Models\LanguageRepository');
+        $this->app->bind('App\Contracts\Models\PublisherInterface', 'App\Repositories\Models\PublisherRepository');
+        $this->app->bind('App\Contracts\Models\PermissionInterface', 'App\Repositories\Models\PermissionRepository');
+        $this->app->bind('App\Contracts\Models\ApplicationInterface', 'App\Repositories\Models\ApplicationRepository');
+        $this->app->bind('App\Contracts\Models\BookCategoryInterface', 'App\Repositories\Models\BookCategoryRepository');
     }
 
     /**
