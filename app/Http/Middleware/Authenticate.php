4f24d4629a6303027fa820efb5a38fd673455729
Sleepy4k <sarahpalastrin@gmail.com>
2023-03-01 14:22:48 +0700

    chore: authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,43 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\ApiRespons;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+    use ApiRespons;
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+            abort(to_route('login.index'));
+        }
+    }
+
+    /**
+     * Stop all system operation when user not authenticated
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\Middleware\Authenticate  $guards
+     * @return string|null
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        if ($request->expectsJson()) {
+            $response = $this->createResponse(trans('api.unauthenticate.error'), [
+                'error' => trans('api.unauthenticate.message')
+            ], 401);
+
+            abort($response);
+        } else {
+            abort(to_route('login.index'));
+        }
     }
 }
