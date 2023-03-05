f60a7d5d1dd4523d13bcb66c74479b732edf1bae
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-06 00:05:07 +0700

    chore: some service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -25,8 +25,10 @@ class EventServiceProvider extends ServiceProvider
 
     /**
      * Register any events for your application.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         // Models Observer
         Models\User::observe(Observers\UserObserver::class);
@@ -40,8 +42,10 @@ class EventServiceProvider extends ServiceProvider
 
     /**
      * Determine if events and listeners should be automatically discovered.
+     *
+     * @return bool
      */
-    public function shouldDiscoverEvents(): bool
+    public function shouldDiscoverEvents()
     {
         return true;
     }
