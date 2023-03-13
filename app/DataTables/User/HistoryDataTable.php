9319bfe0c7eae0db170dea59548c3c21fcc56584
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-14 01:39:08 +0700

    feat: some data table

---
+++ app/DataTables/User/HistoryDataTable.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\DataTables\User;
+
+use App\Models\Borrow;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Services\DataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+
+class HistoryDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     * @return \Yajra\DataTables\EloquentDataTable
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function ($query) {
+                return '<a href="' . route("books.show", $query->id) . '"><i class="fa-solid fa-eye"></i></a>';
+            })
+            ->editColumn('user_id', function ($query) {
+                return $query->user->name;
+            })
+            ->editColumn('book_id', function ($query) {
+                return $query->book->title;
+            })
+            ->setRowId('id');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\Borrow $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Borrow $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+            ->setTableId('history-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->dom('lfrtip')
+            ->orderBy(0, 'asc')
+            ->lengthChange(true)
+            ->lengthMenu()
+            ->pageLength(10)
+            ->responsive(true)
+            ->autoWidth(true)
+            ->selectStyleSingle();
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     *
+     * @return array
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make('user_id')
+                ->hidden(),
+            Column::make('book_id')
+                ->title('Book')
+                ->addClass('text-center'),
+            Column::make('status')
+                ->title('Status')
+                ->addClass('text-center'),
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename(): string
+    {
+        return 'History_' . date('YmdHis');
+    }
+}
