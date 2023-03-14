811d774442ad70fd38aa49760452dd517457b094
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-14 13:37:29 +0700

    feat: some view

---
+++ resources/views/layouts/error.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @includeIf('partials.head.meta')
+        @includeIf('partials.head.title')
+        @includeIf('partials.head.icon')
+        @includeIf('partials.head.error.css')
+    </head>
+    <body>
+        <div id="error">
+            <div class="error-page container">
+                <div class="col-md-8 col-12 offset-md-2">
+                    <div class="text-center">
+                        @hasSection('image')
+                            @yield('image')
+                        @endif
+
+                        <h1 class="error-title">
+                            @hasSection('title')
+                                @yield('title')
+                            @endif
+                        </h1>
+
+                        <p class='fs-5 text-gray-600'>
+                            @hasSection('message')
+                                @yield('message')
+                            @endif
+                        </p>
+
+                        @auth
+                            @if (auth()->user()->hasRole('admin'))
+                                <a href="{{ route('dashboard.index') }}" class="btn btn-lg btn-outline-primary mt-3">@lang('error.back_home')</a>
+                            @else
+                                <a href="{{ route('landing') }}" class="btn btn-lg btn-outline-primary mt-3">@lang('error.back_home')</a>
+                            @endif
+                        @else
+                            <a href="{{ route('landing') }}" class="btn btn-lg btn-outline-primary mt-3">@lang('error.back_home')</a>
+                        @endauth
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/partials/head/error/css.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ asset('user/css/main/app.css') }}">
+<link rel="stylesheet" href="{{ asset('user/css/pages/error.css') }}">
+
+@stack('addon-css')
