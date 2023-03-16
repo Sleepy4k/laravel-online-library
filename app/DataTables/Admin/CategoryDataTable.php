229757981fc54ae1051b1f4089c7bc1cadcf1d19
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-16 20:55:18 +0700

    feat: some data table

---
+++ app/DataTables/Admin/CategoryDataTable.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\DataTables\Admin;
+
+use App\Models\BookCategory;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Services\DataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+
+class CategoryDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     * @return \Yajra\DataTables\EloquentDataTable
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function ($query) {
+                return '<a href="' . route("admin.categories.show", $query->id) . '"><i class="fa-solid fa-eye"></i></a> | <a href="' . route('admin.categories.edit', $query->id) . '"><i class="fa-solid fa-pen-to-square"></i></a> | <form action="' . route("admin.categories.destroy", $query->id) . '" method="post" style="display: inline-block">' . csrf_field() . method_field("DELETE") . '<a href="" class="confirm-delete"><i class="fa-solid fa-trash"></i></a></form>';
+            })
+            ->setRowId('id');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\BookCategory $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(BookCategory $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+            ->setTableId('category-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->dom('Bfrtip')
+            ->orderBy(0, 'asc')
+            ->lengthChange(true)
+            ->lengthMenu()
+            ->pageLength(10)
+            ->responsive(true)
+            ->autoWidth(true)
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('create'),
+                Button::make('export'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     *
+     * @return array
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make('name')
+                ->title('Name')
+                ->addClass('text-center'),
+            Column::computed('action')
+                ->title('Action')
+                ->exportable(false)
+                ->printable(false)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename(): string
+    {
+        return 'Category_' . date('YmdHis');
+    }
+}
