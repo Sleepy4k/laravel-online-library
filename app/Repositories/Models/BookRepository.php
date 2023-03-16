7d48b5631cc81d3bedf75e560b21e04c966e0104
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-16 20:55:58 +0700

    chore: book repository

--- app/Repositories/Models/BookRepository.php
+++ app/Repositories/Models/BookRepository.php
@@ -3,12 +3,15 @@
 namespace App\Repositories\Models;
 
 use App\Models\Book;
+use App\Traits\UploadFile;
 use App\Contracts\Models\BookInterface;
 use Illuminate\Database\Eloquent\Model;
 use App\Repositories\EloquentRepository;
 
 class BookRepository extends EloquentRepository implements BookInterface
 {
+    use UploadFile;
+
     /**
      * @var Model
      */
@@ -22,5 +25,64 @@ class BookRepository extends EloquentRepository implements BookInterface
     public function __construct(Book $model)
     {
         $this->model = $model;
+    }
+
+    /**
+     * Create a model.
+     *
+     * @param  array  $payload
+     * @return Model
+     */
+    public function create(array $payload): ?Model
+    {
+        try {
+            $payload['image'] = $this->saveSingleFile('image', $payload['image']);
+            $model = $this->model->create($payload);
+
+            return $model->fresh();
+        } catch (\Throwable $th) {
+            $this->sendReportLog('error', $th->getMessage());
+
+            return false;
+        }
+    }
+
+    /**
+     * Update existing model.
+     *
+     * @param  int  $modelId
+     * @param  array  $payload
+     * @return Model
+     */
+    public function update(int $modelId, array $payload): bool
+    {
+        try {
+            $payload['image'] = $this->updateSingleFile('image', $payload['image'], $payload['old_image']);
+            $model = $this->findById($modelId);
+
+            return $model->update($payload);
+        } catch (\Throwable $th) {
+            $this->sendReportLog('error', $th->getMessage());
+
+            return false;
+        }
+    }
+
+    /**
+     * Delete model by id.
+     *
+     * @param  int  $modelId
+     * @return Model
+     */
+    public function deleteById(int $modelId): bool
+    {
+        try {
+            $this->deleteFile('image', $this->findById($modelId)->image);
+            return $this->findById($modelId)->delete();
+        } catch (\Throwable $th) {
+            $this->sendReportLog('error', $th->getMessage());
+
+            return false;
+        }
     }
 }
